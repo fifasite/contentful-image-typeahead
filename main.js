55b3d86545919f1bff657cd453ed752e1b3e7b6c
Thomas Kokholm <thomas@Thomass-MacBook-Pro.local>
2020-03-18 16:06:10 +0100

    Added search with suggestions

--- main.js
+++ main.js
@@ -1,6 +1,63 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Instance'
+        query: null,
+        players: [
+            { id: 1, name: "Lionel Messi", imgUrl: "/img/messi.jpg" },
+            { id: 2, name: "Cristiano Ronaldo", imgUrl: "/img/ronaldo.jpg" },
+            { id: 3, name: "Diego Maradona", imgUrl: "/img/maradona.jpg" },
+            { id: 4, name: "Zinedine Zidane", imgUrl: "/img/zidane.jpg" },
+            { id: 5, name: "Pele Edson Arantes do Nascimento", imgUrl: "/img/pele.jpg" },
+            { id: 6, name: "Johan Cruyff", imgUrl: "/img/cruyff.jpg" },
+            { id: 7, name: "Ronaldinho Ronaldo de Assis Moreira", imgUrl: "/img/ronaldinho.jpg" }
+        ],
+        selectedPlayers: [],
+        suggestions: []
+    },
+    computed: {
+        showSuggestions() {
+            if (this.suggestions.length > 0) {
+                return true;
+            }
+            return false;
+        },
+        showSelectedPlayers() {
+            if (this.selectedPlayers.length > 0) {
+                return true;
+            }
+            return false;
+        }
+    },
+    mounted() {
+        for (let player of this.players) {
+            const str = player.name.toLowerCase();
+            player['q'] = str.split(" ");
+        }
+        this.resetSuggestions();
+    },
+    methods: {
+        resetSuggestions() {
+            this.suggestions = this.players;
+        },
+        search() {
+            if (this.query === "") {
+                this.resetSuggestions();
+            } else {
+                this.suggestions = [];
+                for (let player of this.players) {
+                    //console.log("player q: ", player.q);
+                    //console.log(player.q.indexOf("lionel"));
+                    if (player.q.indexOf(this.query.toLowerCase()) > -1) {
+                        console.log("got one or more matches", player.id);
+                        this.suggestions.push(player);
+                    }
+                }
+            }
+
+            //this.suggestions.push(this.players[0]);
+        },
+        selectPlayer(player) {
+            this.selectedPlayers.push(player);
+        }
     }
 })
