95d8f831334bc8f994b9f437db156cfd2737bfd4
Thomas Kokholm <thomas@Thomass-MacBook-Pro.local>
2020-03-19 09:40:45 +0100

    New way of initializing ui extension

--- main.js
+++ main.js
@@ -1,124 +1,122 @@
-let FIELD = null;
-new Vue({
-    el: '#app',
-    data: {
-        query: null,
-        players: [
-            { id: 1, name: "Lionel Messi", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/messi.jpg" },
-            { id: 2, name: "Cristiano Ronaldo", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/ronaldo.jpg" },
-            { id: 3, name: "Diego Maradona", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/maradona.jpg" },
-            { id: 4, name: "Zinedine Zidane", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/zidane.jpg" },
-            { id: 5, name: "Pele Edson Arantes do Nascimento", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/pele.jpg" },
-            { id: 6, name: "Johan Cruyff", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/cruyff.jpg" },
-            { id: 7, name: "Ronaldinho Ronaldo de Assis Moreira", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/ronaldinho.jpg" }
-        ],
-        selectedPlayers: [],
-        suggestions: []
-    },
-    computed: {
-        showSuggestions() {
-            if (this.suggestions.length > 0) {
-                return true;
-            }
-            return false;
+
+window.contentfulExtension.init(function (sdk) {
+    // Automatically adjust UI Extension size in the Web App.
+    sdk.window.startAutoResizer();
+
+    // Get current value of field
+    let value = sdk.field.getValue()
+    console.log("[contentful-image-typeahead] value:", value);
+
+    // Set value of field to "Hello World!"
+    //sdk.field.setValue("Hello world!")
+
+
+    new Vue({
+        el: '#app',
+        data: {
+            query: null,
+            players: [
+                { id: 1, name: "Lionel Messi", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/messi.jpg" },
+                { id: 2, name: "Cristiano Ronaldo", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/ronaldo.jpg" },
+                { id: 3, name: "Diego Maradona", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/maradona.jpg" },
+                { id: 4, name: "Zinedine Zidane", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/zidane.jpg" },
+                { id: 5, name: "Pele Edson Arantes do Nascimento", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/pele.jpg" },
+                { id: 6, name: "Johan Cruyff", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/cruyff.jpg" },
+                { id: 7, name: "Ronaldinho Ronaldo de Assis Moreira", imgUrl: "https://thomaskokholm.github.io/contentful-image-typeahead/img/ronaldinho.jpg" }
+            ],
+            selectedPlayers: [],
+            suggestions: []
         },
-        showSelectedPlayers() {
-            if (this.selectedPlayers.length > 0) {
-                return true;
+        computed: {
+            showSuggestions() {
+                if (this.suggestions.length > 0) {
+                    return true;
+                }
+                return false;
+            },
+            showSelectedPlayers() {
+                if (this.selectedPlayers.length > 0) {
+                    return true;
+                }
+                return false;
+            },
+            jsonsDataString() {
+                if (this.selectedPlayers.length > 0) {
+                    return JSON.stringify(this.selectedPlayers);
+                }
+                return "";
             }
-            return false;
         },
-        jsonsDataString() {
-            if (this.selectedPlayers.length > 0) {
-                return JSON.stringify(this.selectedPlayers);
+        mounted() {
+            // When UI Extensions SDK is loaded the callback will be executed.
+
+
+            for (let player of this.players) {
+                const str = player.name.toLowerCase();
+                player['q'] = str.split(" ");
             }
-            return "";
-        }
-    },
-    mounted() {
-        // When UI Extensions SDK is loaded the callback will be executed.
-        window.contentfulExtension.init(initExtension);
-
-        function initExtension(extensionsApi) {
-            // "extensionsApi" is providing an interface documented here:
-            // https://github.com/contentful/ui-extensions-sdk/blob/master/docs/ui-extensions-sdk-frontend.md
-
-            // Automatically adjust UI Extension size in the Web App.
-            extensionsApi.window.startAutoResizer();
-
-            //  The FIELD this UI Extension is assigned to.
-            FIELD = extensionsApi.field;
-            console.log("FIELD", FIELD);
-        }
-
-        for (let player of this.players) {
-            const str = player.name.toLowerCase();
-            player['q'] = str.split(" ");
-        }
-        this.resetSuggestions();
-    },
-    methods: {
-        resetSuggestions() {
-            //this.suggestions = this.players;
-            this.suggestions = [];
+            this.resetSuggestions();
         },
-        search() {
-            if (this.query === "") {
-                this.resetSuggestions();
-            } else {
+        methods: {
+            resetSuggestions() {
+                //this.suggestions = this.players;
                 this.suggestions = [];
-                for (let player of this.players) {
-                    /*
-                    if (player.q.indexOf(this.query.toLowerCase()) > -1) {
-                        console.log("got one or more matches", player.id);
-                        this.suggestions.push(player);
-                    }
-                    */
-                    for (let token of player.q) {
-                        if (token.search(this.query) > -1) {
-                            this.addSuggestion(player);
+            },
+            search() {
+                if (this.query === "") {
+                    this.resetSuggestions();
+                } else {
+                    this.suggestions = [];
+                    for (let player of this.players) {
+                        /*
+                        if (player.q.indexOf(this.query.toLowerCase()) > -1) {
+                            console.log("got one or more matches", player.id);
+                            this.suggestions.push(player);
+                        }
+                        */
+                        for (let token of player.q) {
+                            if (token.search(this.query) > -1) {
+                                this.addSuggestion(player);
+                            }
                         }
                     }
                 }
-            }
 
-            //this.suggestions.push(this.players[0]);
-        },
-        selectPlayer(selected) {
-            this.resetSuggestions();
-            this.query = null;
+                //this.suggestions.push(this.players[0]);
+            },
+            selectPlayer(selected) {
+                this.resetSuggestions();
+                this.query = null;
 
-            let found = false;
-            for (let player of this.selectedPlayers) {
-                if (player == selected.name) {
-                    found = true;
+                let found = false;
+                for (let player of this.selectedPlayers) {
+                    if (player == selected.name) {
+                        found = true;
+                    }
                 }
-            }
-            if (!found) {
-                this.selectedPlayers.push(selected.name);
-                if (FIELD) {
-                    FIELD.setValue(JSON.stringify(this.selectedPlayers));
-                } else {
-                    console.error("FIELD is null");
+                if (!found) {
+                    this.selectedPlayers.push(selected.name);
+                    sdk.field.setValue(JSON.stringify(this.selectedPlayers));
                 }
-
-            }
-        },
-        addSuggestion(suggestedPlayer) {
-            let found = false;
-            for (let player of this.suggestions) {
-                if (player.id == suggestedPlayer.id) {
-                    found = true;
+            },
+            addSuggestion(suggestedPlayer) {
+                let found = false;
+                for (let player of this.suggestions) {
+                    if (player.id == suggestedPlayer.id) {
+                        found = true;
+                    }
                 }
+                if (!found) {
+                    this.suggestions.push(suggestedPlayer);
+                }
+            },
+            removeSelectedPlayer(selected) {
+                this.selectedPlayers = this.selectedPlayers.filter(function (obj) {
+                    return obj.name !== selected.name;
+                });
             }
-            if (!found) {
-                this.suggestions.push(suggestedPlayer);
-            }
-        },
-        removeSelectedPlayer(selected) {
-            this.selectedPlayers = this.selectedPlayers.filter(function (obj) {
-                return obj.name !== selected.name;
-            });
         }
-    }
-})
+    });
+
+
+});
